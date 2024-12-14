f0d56bc3a649a1704c386b9f00b42428971faef4
dee797 <178827694+dee797@users.noreply.github.com>
2024-12-14 15:45:20 -0700

    Add instructions and create context for state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import Card from "./Card";
 
 const useFetchItems = () => {
@@ -24,29 +24,39 @@ const useFetchItems = () => {
   return { items, error, loading };
 };
 
-
+export const CardContext = createContext(null)
 
 const App = () => {
   const { items, error, loading } = useFetchItems();
-  const [ itemIndexes, randomizeItemIndexes ] = useState([20, 21, 22, 35, 36, 37, 38, 39, 40, 67, 68, 69]);
+  const [ itemNumbers, shuffleItemNumbers ] = useState([20, 21, 22, 35, 36, 37, 38, 39, 40, 67, 68, 69]);
 
   if (loading) return (<div className="h-screen w-screen flex items-center justify-center"><div className="loader mx-auto"></div></div>);
   if (error) return (<p className="h-screen w-screen text-center">A network error was encountered.</p>);
 
   return (
-    <div className="flex flex-col max-w-full min-h-screen">
-      <header className="px-4 bg-black">
-        <div className="relative mx-auto py-6 flex max-w-screen-lg flex-col sm:flex-row sm:items-center sm:justify-between text-neutral-50">
-          <span>Breath of the Wild Memory Card Game</span>
-          <span>Score: {}</span>
-          <span>Best Score: {}</span>
+    <div className="flex flex-col max-w-full min-h-screen bg-yellow-50">
+      <header className="p-8 bg-sky-900 drop-shadow-2xl text-white">
+        <div className="mx-40 py-6 max-w-full flex flex-col sm:flex-row sm:items-center sm:justify-between ">
+          <span className="font-bold text-4xl">Breath of the Wild: Memory Card Game</span>
+          <div className="flex sm:justify-end gap-20 text-2xl">
+            <span>Score: {}</span>
+            <span>Best Score: {}</span>
+          </div>
+        </div>
+        <div className="flex flex-col mx-40 pb-6 gap-x-2 text-xl">
+          <p className="font-bold text-2xl">How to Play:</p>
+          <p>1. Choose from any of the magical weapons below. Make sure to remember which one you picked.</p>
+          <p>2. The weapons will shuffle. Pick a different weapon, making sure not to select one that you've already clicked before.</p>
+          <p>3. Repeat until you've selected all the weapons exactly once. When you've reached this condition, you win the game!</p>
         </div>
       </header>
 
-      <div className="grid grid-rows-3 grid-cols-4 w-full">
-        {itemIndexes.map((item) => {
-          return <Card item={items.data[item]} key={item}/>
-        })}
+      <div className="grid grid-cols-5 grid-rows-3 max-w-full mx-5 my-16 gap-y-14">
+        <CardContext.Provider value={{shuffleItemNumbers}} >
+          {itemNumbers.map((itemNum) => {
+            return <Card item={items.data[itemNum]} key={itemNum} />
+          })}
+        </CardContext.Provider>
       </div>
 
     </div>
